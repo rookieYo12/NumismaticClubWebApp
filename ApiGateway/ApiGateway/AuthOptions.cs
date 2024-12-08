cba72185e7a9212164fdc862bcc1856a43464059
Andrey Kharitonov <a.kharitonovt.rex@gmail.com>
2024-12-08 14:39:24 +0300

    added some jwt auth options to api gateway

---
+++ ApiGateway/ApiGateway/AuthOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiGateway
+{
+    public class AuthOptions
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string Key { get; set; }
+        public int Lifetime { get; set; }
+    }
+}
--- ApiGateway/ApiGateway/Program.cs
+++ ApiGateway/ApiGateway/Program.cs
@@ -1,12 +1,40 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using ApiGateway;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Ocelot configuration file
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+// And then add Ocelot
 builder.Services.AddOcelot(builder.Configuration);
 
+// Get auth options from app config file
+var authOptionsSection = builder.Configuration.GetSection("AuthOptions");
+builder.Services.Configure<AuthOptions>(authOptionsSection);
+var authOptions = authOptionsSection.Get<AuthOptions>();
+
+// Add authenntication scheme and jwt token validation parameters
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(configureOptions =>
+    {
+        configureOptions.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = authOptions.Issuer,
+            ValidateAudience = true,
+            ValidAudience = authOptions.Audience,
+            ValidateLifetime = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(authOptions.Key)),
+            ValidateIssuerSigningKey = true,
+        };
+    });
 
 var app = builder.Build();
 
